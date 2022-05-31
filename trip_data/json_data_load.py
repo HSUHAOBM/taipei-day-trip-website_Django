3df6c7b0c59130e6756b2a910bf1b19813c868b8
HSUHAOBM <hao66bmbm@gmail.com>
2022-05-31 10:55:28 +0800

    feat: json file load
    trip_data

---
+++ trip_data/json_data_load.py
@@ -0,0 +1,59 @@
+import json
+import re
+import os
+import django
+from django.core.wsgi import get_wsgi_application
+
+import sys
+
+def main():
+    updata = []
+
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    print(parent_dir)
+
+    with open("data/taipei-attractions.json", "r", encoding="utf-8") as json_data:
+        data = json.load(json_data)
+
+    def getimg(a):  # 圖片篩選
+        reg = "http.*?\.jpg"
+        imgre = re.compile(reg)
+        imglist_jgp = imgre.findall(a)
+        #     print("imglist_jpg:",imglist_jgp)
+        reg2 = "http.*?\.png"
+        imgre2 = re.compile(reg2)
+        imglist_png = imgre2.findall(a)
+        #     print("imglist2_png:",imglist_png)
+        return imglist_jgp + imglist_png
+
+    for i in range(len(data["result"]["results"])):
+        models.Trip_data.objects.create(
+            stitle = data["result"]["results"][i]["stitle"],
+            category = data["result"]["results"][i]["CAT2"],
+            description = data["result"]["results"][i]["xbody"],
+            address = "".join(data["result"]["results"][i]["address"].split(" ")[0::2]),
+            transport = data["result"]["results"][i]["info"],
+            mrt = data["result"]["results"][i]["MRT"],
+            latitude = data["result"]["results"][i]["latitude"],
+            longitude = data["result"]["results"][i]["longitude"],
+            images = ",".join(
+                getimg(
+                    ",http://".join(
+                        data["result"]["results"][i]["file"].lower().split("http://")
+                    )
+                )
+            ),
+        )
+
+
+
+if __name__ == "__main__":
+    sys.path.append("/home/sl/test/learn/Django/taipei_trip/")
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taipei_trip.settings")
+    application = get_wsgi_application()
+
+    django.setup()
+    from trip_data import models
+
+    main()
