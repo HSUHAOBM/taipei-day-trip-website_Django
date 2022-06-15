a6034eceba0ae84130c1ea64850ff52f2fffe34f
HSUHAOBM <hao66bmbm@gmail.com>
2022-06-15 15:14:20 +0800

    feat: attraction web

--- trip_data/views.py
+++ trip_data/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponseBadRequest, JsonResponse
 from trip_data.models import Trip_data
+from django.forms.models import model_to_dict
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 # Create your views here.
 from django.http import HttpResponse
@@ -22,6 +27,26 @@ def trip_data(request,num=0,keyword=None):
             else:
                 nextpage = None
             return JsonResponse({'data': trip_data,"nextpage": nextpage})
+        return JsonResponse({'status': 'Invalid request'}, status=400)
+    else:
+        return HttpResponseBadRequest({'error':True})
+
+
+def attraction_web(request,num):
+    return render(request, 'attraction.html')
+
+def attraction(request,num):
+    is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+
+    if is_ajax:
+        if request.method == 'GET':
+            try:
+                trip_data = Trip_data.objects.get(pk=num)
+                trip_data = model_to_dict(trip_data)
+            except Trip_data.DoesNotExist:
+                trip_data = None
+            return JsonResponse({'data': trip_data,})
+
         return JsonResponse({'status': 'Invalid request'}, status=400)
     else:
         return HttpResponseBadRequest({'error':True})
