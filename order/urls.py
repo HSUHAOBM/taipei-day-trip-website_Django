892662a1ae12408b71345d6e1bdb493f1b256e99
Hao <hao66bmbm@gmail.com>
2022-06-26 23:13:06 +0800

    feat: order

---
+++ order/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from order import views
+
+
+app_name = 'order'
+urlpatterns = [
+    path('booking/', views.main),
+    path('api/book', views.book),
+    path('api/order', views.order),
+    path('thanks/number=<int:num>', views.thanks)
+
+]
+
+
