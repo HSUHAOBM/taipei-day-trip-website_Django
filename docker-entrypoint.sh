2c45573be42ea5422807401478019e14137e0f13
Ian <ian@ian-18.04>
2022-07-02 23:41:12 +0800

    feat: docker sh modify

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 echo 'Run migration'
-python3 manage.py makemigrations
+
+python3 manage.py makemigrations order
+python3 manage.py makemigrations member
+python3 manage.py makemigrations trip_data
 python3 manage.py migrate
 echo 'Create Super User'
 python3 manage.py createsuperuser --noinput || echo "Super user already created"
 exec "$@"
+cd  trip_data
+python json_data_load.py
+pwd
+
+
+
+
